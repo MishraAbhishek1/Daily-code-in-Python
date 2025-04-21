547edba0109d748711be09c293f5903e59bd7b34
KumarAbhishek67 <kumarmishraa608@gmail.com>
2025-04-21 18:29:19 +0530

    add a one more Program

--- loops/intro.py
+++ loops/intro.py
@@ -2,7 +2,31 @@
 #1. while loop
 #2. for loop
 
+# i = 1
+# while i<=10:
+#     print(i)
+#     i +=1
+
+print("Hello World")
+
+#break and continue
+1 = 1
+while i<=10:
+    print(i)
+    if (i == 5):
+        break
+    i += 1
+
 i = 1
 while i<=10:
     print(i)
-    i +=1
+    if(i == 5):
+        continue
+    i += 1  
+
+i = 0
+while i<9:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 9")          
