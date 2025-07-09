981a571361aa657c0f09945d47559eaa698b09ba
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-09 11:12:31 +0530

    working on list

---
+++ List/chapt11.py
@@ -0,0 +1,5 @@
+# when we get list index number and enumerate()
+
+fruits = ["apple", "banana", "mango"]
+for i, fruit in enumerate(fruits):
+    print(f"Index {i}: {fruit}")
---
+++ List/chapt14.py
@@ -0,0 +1,5 @@
+students = ["abhi", "naryana", "Mona", "stayjeet"]
+result1 = tuple(enumerate(students))
+result2 = list(enumerate(students))
+print(result1)
+print(result2)
