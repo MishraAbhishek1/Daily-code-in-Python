e23421366a2396bcb21c08b53a691c1a007a8967
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-11 16:08:56 +0530

    start new method conditional

---
+++ condition/chapt1.py
@@ -0,0 +1,13 @@
+# conditional statemnets are a block of a code that executes a certin of code based on condition
+
+age = 66
+
+# apply conditional statement
+if age<18:
+    print("You are a minor")
+else:
+    print("You are an adult")    
+
+# if condition is a simplest form of a conditional statement if condition is true then it will execute the block of code
+
+
