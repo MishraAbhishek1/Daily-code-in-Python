1d744817cb3ea912dab5450c9cec75942c5f2cb4
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-15 10:28:20 +0530

    new changes

---
+++ condition/chapt10.py
@@ -0,0 +1,12 @@
+# if -elif-else Ladder 
+marks = int(input("Enter your marks:"))
+if marks >= 90:
+    print("grade A")
+elif marks >= 80:
+    print("grade B")
+elif marks >= 70:
+    print("grade C")
+elif marks >= 60:
+    print("grade D")
+else:
+    print("grade F")                
---
+++ condition/chapt9.py
@@ -0,0 +1,20 @@
+# why we use condition >
+""" ❓ Why use condition?
+
+Because real-world applications me decision-making zaruri hota hai.
+🔸 🔥 Without condition — no logic:
+
+    Aap login banaye bina condition ke?
+
+    Admin aur user ka alag page kaise dikhaoge?
+
+    Payment successful/failed kaise bataoge?
+
+    User input galat hai ya sahi kaise verify karoge?"""
+
+# Example of a simple condition
+raining = False
+if raining:
+    print("take an Umberalla")
+else:
+    print("No need for an Umberalla")    
