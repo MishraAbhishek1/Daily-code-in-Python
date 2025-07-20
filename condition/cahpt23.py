49c7e42b7a7b2c10dda5c8fd81060c4de5c5db00
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-20 16:03:08 +0530

    new changes

---
+++ condition/cahpt23.py
@@ -0,0 +1,10 @@
+# ✅ Problem 22: Profile Completion Check
+
+name = "Abhishek"
+email = "grgrg"
+phone = "123456789"
+
+if name and email and phone:
+    print("Profile Completed")
+else:
+    print("Incomplete Profile")
