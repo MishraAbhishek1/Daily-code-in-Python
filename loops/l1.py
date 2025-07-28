b0d3f28350904974f693d63ac4e37ea459068e48
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-28 10:39:38 +0530

    start learning loop

---
+++ loops/l1.py
@@ -0,0 +1,7 @@
+# what is loop >  loops in python repeat atask in efficientely
+'''The Main types are four types of loops (counting through items ) and while loops based on the conditions
+and for loop is a iterate over a sequence such as a lit tuple and string or range it allow to execute a block of code repeteadly '''
+
+n = 4
+for i in range(1, n+1):
+    print(i)
