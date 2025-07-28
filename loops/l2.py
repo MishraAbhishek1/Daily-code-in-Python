2fea998e57b7d28cb3a845bd49460f1fb2404c0c
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-28 10:39:38 +0530

    start learning loop

---
+++ loops/l2.py
@@ -0,0 +1,24 @@
+# loop over list
+list = ["geeks","harry","freecode","campus x"]
+for i in list:
+    print(i)
+
+# loop over tuple:
+tuple = ("geeks", "for", "geeks")
+for i in tuple:
+    print(i)
+
+# loop over String
+s = "Geeks"
+for i in s:
+    print(i) 
+
+# loop over dictionary
+d = dict({"age": 123, "rollno": 123})
+for i in d:
+    print("%s  %d" % (i, d[i]))
+
+# loop over set
+s = {1,2,3,4,5}
+for i in s:
+    print(i)
