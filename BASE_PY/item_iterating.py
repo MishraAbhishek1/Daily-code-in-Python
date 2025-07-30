7803c1899ba1897c824e963c86e2fef509366f92
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-07-30 18:53:18 +0530

    continue a Basic .py folder basic python code learning and practice

---
+++ BASE_PY/item_iterating.py
@@ -0,0 +1,11 @@
+#item iterating - Through the items in sequence
+
+#item
+x = [1,2,3,4,5,6,7]
+for i in x:
+    print(i)
+print('\n')
+# index & item 
+y = [7, 8, 3]
+for ind, i in enumerate(y):
+    print(ind, i)    
---
+++ BASE_PY/lexicographycally.py
@@ -0,0 +1,14 @@
+# minimun - Find the minimum item in a sequence lexicographically.
+# Alpha or numeric types, but cannot mix types
+
+#string
+x = 'bug'
+print(min(x))
+
+#list
+y = ['gfjkgbkj','bgfugb', 'gfgn']
+print(min(y)),
+
+#tuple
+z = ('kelvin', 'nikals', 'Jenny', 'Craig')
+print(min(z))
