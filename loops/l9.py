0e1842daebb1dfb58826eb3b95df79c13432c290
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-08-04 11:13:05 +0530

    again statrt in Python loop Journey

---
+++ loops/l9.py
@@ -0,0 +1,10 @@
+products = ["iphone", "oppo", "vivo", "motoralla"]
+
+output = []
+
+for product in products:
+    line = f"Processing product: {product}"
+    output.append(line)
+
+print("\n".join(output))
+
