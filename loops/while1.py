13d6ea1c8546380024079b52d94685c0be531fad
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-08-04 16:48:01 +0530

    new changes

---
+++ loops/while1.py
@@ -0,0 +1,11 @@
+# 2. while Loop
+# The while loop runs as long as a condition is True.
+# Syntax:
+# pythonwhile condition:
+#     # Code block
+
+count = 1
+
+while count <= 5:
+    print(f"Count is: {count}")
+    count += 1
