247ee80ee57856ba392db3cd041f75f3250f4fde
MishraAbhishek1 <kumarmishraa608@gmail.com>
2025-08-05 17:57:51 +0530

    In Progress Learning in loop concepts

---
+++ loops/l13.py
@@ -0,0 +1,6 @@
+# Example 8: Pairing Data
+
+names = ['Alice', 'bob', 'Cahrile']
+scores = [85, 90, 100]
+for name, score in zip(names, scores):
+    print(f"{name} Scored {score}")
---
+++ loops/l14.py
@@ -0,0 +1,9 @@
+# Part 3: Advanced Loop Concepts
+# Let’s dive into advanced techniques used in real-world projects.
+# 7. List Comprehensions
+# A concise way to create lists using loops.
+
+# Example 9: Squaring Numbers
+
+squares = [x**2 for x in range(5)]
+print(squares)
