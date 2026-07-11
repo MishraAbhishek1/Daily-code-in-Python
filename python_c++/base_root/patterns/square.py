a4a6f131629d5465a69237aa711fd806ea3c43fc
KumarAbhishek67 <mabhishek@weboappdiscovery.com>
2026-07-12 00:17:15 +0530

    start learnign loops fundamentals

---
+++ python_c++/base_root/patterns/square.py
@@ -0,0 +1,17 @@
+# print the square nnumebr having  a n= 4 value
+
+# 1234
+# 1234
+# 1234
+
+def square_patterns(n):
+    
+    for i in range(0, n+1):
+        
+        for j in range(0, n+1):
+            
+            print(j, end=" ")
+            
+        print()        
+
+square_patterns(4)
